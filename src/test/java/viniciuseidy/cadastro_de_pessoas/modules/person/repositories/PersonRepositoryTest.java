52527f575a1d139475127be20fb5ed533f353997
eidynho <zviniciuseidy2@gmail.com>
2024-06-15 18:41:01 -0300

    feat: person repository find by cpf tests

---
+++ src/test/java/viniciuseidy/cadastro_de_pessoas/modules/person/repositories/PersonRepositoryTest.java
@@ -0,0 +1,63 @@
+package viniciuseidy.cadastro_de_pessoas.modules.person.repositories;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.persistence.EntityManager;
+import viniciuseidy.cadastro_de_pessoas.modules.person.dto.CreatePersonRequestDTO;
+import viniciuseidy.cadastro_de_pessoas.modules.person.entities.PersonEntity;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class PersonRepositoryTest {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    EntityManager entityManager;
+    
+    @Test
+    @DisplayName("Should get Person successfully from database")
+    void findByCpfSuccess() {
+        String cpf = "12345678900";
+        String birthDateStr = "2001-09-02";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate birthDate = LocalDate.parse(birthDateStr, formatter);
+
+        CreatePersonRequestDTO data = new CreatePersonRequestDTO("Vinicius", cpf, birthDate);
+
+        this.createPerson(data);
+
+        Optional<PersonEntity> result = this.personRepository.findByCpf(cpf);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get Person from database when person does not exists.")
+    void findByCpfError() {
+        String cpf = "12345678900";
+
+        Optional<PersonEntity> result = this.personRepository.findByCpf(cpf);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private PersonEntity createPerson(CreatePersonRequestDTO data) {
+        PersonEntity newPerson = new PersonEntity(data);
+
+        this.entityManager.persist(newPerson);
+
+        return newPerson;
+    }
+}
